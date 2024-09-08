6c69866594f9be1236b84db9f0f7a5f3602291c1
Adrian <adrian@hostpress.de>
2024-09-08 09:56:00 +0200

    Kleinere Fehlerkorrektur (Abfrage ob eine Datei übergeben wurde)

--- umlaute.sh
+++ umlaute.sh
@@ -3,25 +3,30 @@
 # Autor: Adrian Feld (adrian@adminquest.de)
 # Syntax: umlaute.sh <meinedatei.html> z. B. ./umlaute.sh index.html
 
+# Überprüfen, ob eine Datei übergeben wurde
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <meinedatei.html>"
+    exit 1
+fi
+
 # Kein Backup kein Mitleid
-cp $1 $1.bak             
+cp "$1" "$1.bak"
 
 # HTML Umstellung
-sed -i -e 's/\Ä/\&Auml;/g' \
-    -i -e 's/\ä/\&auml;/g' \
-    -i -e 's/\Ö/\&Ouml;/g' \
-    -i -e 's/\ö/\&ouml;/g' \
-    -i -e 's/\Ü/\&Uuml;/g' \
-	-i -e 's/\ß/\&szlig;/g' \
-    -i -e 's/\ü/\&uuml;/g' \
-    -i -e 's/\^&/\&amp;/g' \
-    -i -e 's/\^-/\&ndash;/g' \
-    -i -e 's/\^ /\&nbsp;/g' \
-    -i -e 's/\^</\&lt;/g' \
-    -i -e 's/\^>/\&gt;/g' \
-    -i -e 's/\^x/\&times;/g' \
-    -i -e 's/\^\^/\&#94;/g' \
-    -i -e 's/\^r/\&rarr;/g' \
-    -i -e 's/\^b/\&bull;/g' \
-    -i -e 's/\^e/\&#8644;/g' \
-$1
+sed -i -e 's/Ä/&Auml;/g; 
+            s/ä/&auml;/g; 
+            s/Ö/&Ouml;/g; 
+            s/ö/&ouml;/g; 
+            s/Ü/&Uuml;/g; 
+            s/ß/&szlig;/g; 
+            s/ü/&uuml;/g; 
+            s/&/&amp;/g; 
+            s/-/&ndash;/g; 
+            s/ /&nbsp;/g; 
+            s/</&lt;/g; 
+            s/>/&gt;/g; 
+            s/x/&times;/g; 
+            s/\^/&circ;/g; 
+            s/r/&rarr;/g; 
+            s/b/&bull;/g; 
+            s/e/&euro;/g;' "$1"
