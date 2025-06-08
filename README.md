88b1d3edbfbf84233829ad0a9543875d8f9f8f54
Dein Name <deine.email@example.com>
2025-06-08 22:47:24 +0200

    Update umlaute.sh to improve readability and maintainability: - Added ISO 8601 date format to backup filename - Used 'declare' for variable declarations - Moved sed replacements to a separate script file for clarity - Reason: My code was already a bit “primitive” 😃

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ English: This Mini-Bashscript replaces "German umlauts" with HTML code sequences
 German: Dieses Mini-Bashscript ersetzt Deutsche Umlaute durch HTML Code Sequenzen. Praktisch bei typischen Encoding Problemen :-) <br>
 <br>
 # Syntax
- ```./umlaute.sh myfile.txt ```
- 
+ ```./umlaute.sh myfile.html ```
+
  # Notes
  English: No root rights necessary, but the SED  stream editor (http://sed.sourceforge.net/) package must be present.<br>
  German: Keine "root"-Rechte notwendig. SED stream editor (http://sed.sourceforge.net/) Packet muss installiert sein. <br>
--- umlaute.sh
+++ umlaute.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 # Ersetzt Deutsche Umlaute durch HTML Code Sequenzen
-# Autor: Adrian Feld (adrian@adminquest.de)
 # Syntax: umlaute.sh <meinedatei.html> z. B. ./umlaute.sh index.html
 
 # Überprüfen, ob eine Datei übergeben wurde
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <meinedatei.html>"
+    printf "Usage: %s <meinedatei.html>\n" "$0"
     exit 1
 fi
 
-# Kein Backup kein Mitleid
-cp "$1" "$1.bak"
+# Datei und Backup-Name definieren
+declare input_file="$1"
+declare backup_file="${input_file%.html}_$(date +%Y-%m-%d).bak"
+
+# Backup erstellen
+cp "$input_file" "$backup_file"
+
+# Ersetzen von Umlauten und Sonderzeichen in einer separaten Datei
+declare sed_script_file="sed_script.sed"
+
+# Erstellen der sed-Skriptdatei
+cat << 'EOF' > "$sed_script_file"
+s/Ä/&Auml;/g
+s/ä/&auml;/g
+s/Ö/&Ouml;/g
+s/ö/&ouml;/g
+s/Ü/&Uuml;/g
+s/ü/&uuml;/g
+s/ß/&szlig;/g
+s/&/&amp;/g
+s/-/&ndash;/g
+s/ /&nbsp;/g
+s/</&lt;/g
+s/>/&gt;/g
+s/x/&times;/g
+s/\^/&circ;/g
+s/r/&rarr;/g
+s/b/&bull;/g
+s/e/&euro;/g
+EOF
 
 # HTML Umstellung
-sed -i -e 's/Ä/&Auml;/g; 
-            s/ä/&auml;/g; 
-            s/Ö/&Ouml;/g; 
-            s/ö/&ouml;/g; 
-            s/Ü/&Uuml;/g; 
-            s/ß/&szlig;/g; 
-            s/ü/&uuml;/g; 
-            s/&/&amp;/g; 
-            s/-/&ndash;/g; 
-            s/ /&nbsp;/g; 
-            s/</&lt;/g; 
-            s/>/&gt;/g; 
-            s/x/&times;/g; 
-            s/\^/&circ;/g; 
-            s/r/&rarr;/g; 
-            s/b/&bull;/g; 
-            s/e/&euro;/g;' "$1"
+sed -i -f "$sed_script_file" "$input_file"
+
+# Aufräumen: sed-Skriptdatei löschen
+rm "$sed_script_file"
